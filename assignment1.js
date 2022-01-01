1cd5c2be588309f7a1efb0d2f34c56d90a4accac
Adi1432 <69716922+Adi1432@users.noreply.github.com>
2022-01-01 16:28:17 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,7 @@
+console.log("hi");
+var num = 20;
+if (num > 10) {
+  console.log(" num is greater than 10");
+} else {
+  console.log("num is not greater than 10");
+}
---
+++ assignment2.js
@@ -0,0 +1,10 @@
+var a = 1000;
+var b = 20000;
+var c = 30;
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
